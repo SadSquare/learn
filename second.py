874db3288ff41990566a98ec54bcad5ec21662b6
SadSquare <ya.iliagusev1@gmail.com>
2020-07-17 10:57:42 +0300

    Classes exp

---
+++ second.py
@@ -0,0 +1,34 @@
+class Animal:
+    def __init__(self,weight, price):
+        self.weight = weight
+        self.price = price
+
+class No_fly(Animal):
+    fly_status = False
+    
+class Yes_fly(Animal):
+    fly_status = True
+    
+
+
+
+class Cow(No_fly):
+    name = "Cow"
+
+class Goat(No_fly):
+    name = "Goat"
+
+
+class Guse(Yes_fly):
+    name = "Guse"
+
+class Chick(Yes_fly):
+    name = "Chick"
+
+animal_test = Cow(10, 25)
+second_animal_test = Guse(0,0)
+
+print(second_animal_test.name, second_animal_test.fly_status)
+print(animal_test.name, animal_test.fly_status)
+
+
